e5de9de233a0f40d144dba07c353b6180a3b74b6
harshau007 <amanupadhyay2004@gmail.com>
2025-04-30 11:52:28 +0530

    Fixed build issue

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,6 +15,7 @@ const eslintConfig = [
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
       "@next/next/no-img-element": "off",
+      "react/no-unescaped-entities": "off",
     },
   },
 ];
